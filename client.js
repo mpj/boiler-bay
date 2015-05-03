2b9f871151b2993a81be9acf3212f5599220f319
Mattias Petter Johansson <mpj@spotify.com>
2015-05-03 15:10:43 +0200

    add example js client

---
+++ client.js
@@ -0,0 +1,20 @@
+var net = require('net');
+
+var client = net.connect(4444, 'localhost');
+client.setEncoding('utf8');
+setTimeout(function() {
+  client.write('consume mytopic mygroupsz smallest\n');
+}, 500);
+
+setTimeout(function() {
+  client.write('next\n');
+}, 1000)
+
+client.on('data', function(data) {
+  if (data === 'commit-ok\n')
+    client.write('next\n');
+  if (data.indexOf('msg') > -1) {
+    console.log("got msg", data)
+    client.write('commit\n');
+  }
+})
