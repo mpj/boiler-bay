6d7ff6eef76117d13e254f3f4abb54f42aee4b33
Mattias Petter Johansson <mpj@spotify.com>
2015-05-06 08:45:14 +0200

    docker-compose

--- client-consume.js
+++ client-consume.js
@@ -1,6 +1,6 @@
 var net = require('net');
 
-var client = net.connect(4444, 'localhost');
+var client = net.connect(4444, '192.168.99.100');
 client.setEncoding('utf8');
 setTimeout(function() {
   client.write('consume mytopic mygroupsz smallest\n');
@@ -11,7 +11,6 @@ setTimeout(function() {
 }, 1000)
 
 client.on('data', function(data) {
-  console.log('data was', data)
   if (data === 'commit-ok\n')
     client.write('next\n');
   if (data.indexOf('msg') > -1) {
--- client-produce.js
+++ client-produce.js
@@ -1,13 +1,13 @@
 var net = require('net');
 
-var client = net.connect(4444, 'localhost');
+var client = net.connect(4444, '192.168.99.100');
 client.setEncoding('utf8');
 
 setInterval(function() {
   console.log("sending...")
   var msg = Math.floor(Math.random()*10000);
   client.write('send mytopic 123 bajs'+msg+'\n');
-}, 2000)
+}, 250)
 
 client.on('data', function(data) {
   console.log('data was', data)
